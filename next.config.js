630012723f66c41924cf7e1e82ed0428ff7ded7d
fahim-shareear <fahimshareear@gmail.com>
2026-03-17 00:03:45 +0600

    .........

--- next.config.js
+++ next.config.js
@@ -8,7 +8,10 @@ const nextConfig = {
       'lh3.googleusercontent.com',
       'lh4.googleusercontent.com',
       'lh5.googleusercontent.com',
-      'lh6.googleusercontent.com'
+      'lh6.googleusercontent.com',
+      'i.ibb.co.com',
+      'i.ibb.co',
+      'ibb.co'
     ],
   },
 };
